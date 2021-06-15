21b77d8a3e0ee520736ae5f99e9f85dd2d461849
botshen <363088847@qq.com>
2021-06-15 22:22:12 +0800

    remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
-import Button from './components/Button';
-import Box from './components/Box';
 
 
 function App() {
   return (
-    <Box>
-      <Button>你好</Button>
-    </Box>
+    <div>Hello</div>
   );
 }
 
