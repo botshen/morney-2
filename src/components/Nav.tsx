49db50f3709c45e64a5ff425dee42736a35ce311
botshen <363088847@qq.com>
2021-06-17 18:27:28 +0800

    创建 Icon 组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+import Icon from "./icon";
 
-require('icons/money.svg');
-require('icons/tag.svg');
-require('icons/chart.svg');
+
 const NavWrapper = styled.nav`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -32,29 +31,23 @@ const NavWrapper = styled.nav`
 `;
 
 const Nav = () => {
-  return (
-    <NavWrapper>
-      <ul>
-        <li>
-          <svg className="icon">
-            <use xlinkHref="#tag"/>
-          </svg>
-          <Link to="/tags">标签页</Link>
-        </li>
-        <li>
-          <svg className="icon">
-            <use xlinkHref="#money"/>
-          </svg>
-          <Link to="/money">记账页</Link>
-        </li>
-        <li>
-          <svg className="icon">
-            <use xlinkHref="#chart"/>
-          </svg>
-          <Link to="/statistics">统计页</Link>
-        </li>
-      </ul>
-    </NavWrapper>
-  );
+    return (
+        <NavWrapper>
+            <ul>
+                <li>
+                    <Icon name="tag"/>
+                    <Link to="/tags">标签页</Link>
+                </li>
+                <li>
+                    <Icon name="money"/>
+                    <Link to="/money">记账页</Link>
+                </li>
+                <li>
+                    <Icon name="chart"/>
+                    <Link to="/statistics">统计页</Link>
+                </li>
+            </ul>
+        </NavWrapper>
+    );
 };
 export default Nav;
