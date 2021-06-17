d9922cab9d751c9755ca26b1642edad0942c3aa9
botshen <363088847@qq.com>
2021-06-17 23:17:15 +0800

    创建 views 目录

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect
-} from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from 'components/Nav';
-import Layout from 'components/Layout';
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
+import NoMatch from './views/NoMatch';
 
 
 function App() {
   return (
     <Router>
-
       <Switch>
         <Route path="/tags">
           <Tags/>
@@ -30,39 +24,7 @@ function App() {
           <NoMatch/>
         </Route>
       </Switch>
-
     </Router>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>页面不存在</div>
-  );
-}
-
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计页面</h2>
-    </Layout>
-  );
-
-}
-
-function Tags() {
-  return (
-    <Layout>
-      <h2>标签页面</h2>
-    </Layout>
-  );
-}
-
-function Money() {
-  return (
-    <Layout>
-      <h2>记账页面</h2>
-    </Layout>
   );
 }
 
