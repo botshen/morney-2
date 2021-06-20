389ded31ec70c12e663c7b3d292a7d9a1eb14eee
botshen <363088847@qq.com>
2021-06-20 14:21:16 +0800

    封装一个函数组件

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import React from "react"
 
-const TagsSection = styled.section`
+const _TagSection = styled.section`
   background: #FFFFFF;
   padding: 12px 16px;
   flex-grow: 1;
@@ -31,4 +32,19 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `
+const TagsSection: React.FC = () => {
+    return (
+        <_TagSection>
+            <ol>
+                <li>衣</li>
+                <li>食</li>
+                <li>住</li>
+                <li>行</li>
+            </ol>
+            <button>新增标签</button>
+        </_TagSection>
+    )
+}
+
+
 export {TagsSection}
