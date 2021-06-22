20d23ce3956e3f57523c305cf3e6ef3b70d1132e
botshen <363088847@qq.com>
2021-06-22 23:57:15 +0800

    抽离 useTags 函数

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,48 +1,33 @@
 import styled from 'styled-components';
-import React, {useState} from 'react';
+import React from 'react';
+import {useTags} from 'useTags';
 
 const Wrapper = styled.section`
-  background: #FFFFFF;
-  padding: 12px 16px;
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-start;
-
-  > ol {
-    margin: 0 -12px;
-
-    > li {
-      background: #D9D9D9;
-      border-radius: 18px;
-      display: inline-block;
-      padding: 3px 18px;
-      font-size: 14px;
-      margin: 8px 12px;
-
-      &.selected {
+  background: #FFFFFF; padding: 12px 16px;
+  flex-grow: 1; display:flex; flex-direction: column;
+  justify-content: flex-end; align-items: flex-start;
+  > ol { margin: 0 -12px;
+    > li{
+      background: #D9D9D9; border-radius: 18px;
+      display:inline-block; padding: 3px 18px;
+      font-size: 14px; margin: 8px 12px;
+      &.selected{
         background: #f60;
       }
     }
   }
-
-  > button {
-    background: none;
-    border: none;
-    border-bottom: 1px solid #333;
-    padding: 2px 4px;
-    color: #666;
+  > button{
+    background:none; border: none; padding: 2px 4px;
+    border-bottom: 1px solid #333; color: #666;
     margin-top: 8px;
   }
 `;
-
 type Props = {
     value: string[];
     onChange: (selected: string[]) => void;
 }
 const TagsSection: React.FC<Props> = (props) => {
-    const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+    const {tags, setTags} = useTags();
     const selectedTags = props.value;
     const onAddTag = () => {
         const tagName = window.prompt('新标签的名称为');
@@ -53,23 +38,20 @@ const TagsSection: React.FC<Props> = (props) => {
     const onToggleTag = (tag: string) => {
         const index = selectedTags.indexOf(tag);
         if (index >= 0) {
+            // 如果 tag 已被选中，就复制所有没有被选中的 tag，作为新的 selectedTag
             props.onChange(selectedTags.filter(t => t !== tag));
         } else {
             props.onChange([...selectedTags, tag]);
         }
     };
     const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
-
     return (
         <Wrapper>
             <ol>
                 {tags.map(tag =>
                     <li key={tag} onClick={
-                        () => {
-                            onToggleTag(tag);
-                        }
-                    }
-                        className={getClass(tag)}
+                        () => {onToggleTag(tag);}
+                    } className={getClass(tag)}
                     >{tag}</li>
                 )}
             </ol>
