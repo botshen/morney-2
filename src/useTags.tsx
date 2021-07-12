fa70cdc9c5913ec706db844cd1e70a0452ef0368
shenxin <363088847@qq.com>
2021-07-12 16:59:19 +0800

    Revert "创建 createId，让 id 自增"
    This reverts commit 325c6eaf

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {createId} from 'lib/createId';
+import {createId} from './lib/createId';
 
 const useTags = () => { // 封装一个自定义 Hook
     const [tags, setTags] = useState<{ id: number; name: string }[]>([
