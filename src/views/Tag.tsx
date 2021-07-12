551804915b617e5f84ed670fd03152284239dd9e
shenxin <363088847@qq.com>
2021-07-12 18:02:03 +0800

    封装 findTag 到 useTags 里

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,6 +1,15 @@
+import {useTags} from '../useTags';
+import {useParams} from 'react-router-dom';
+
+type Params = {
+    id: string
+}
 const Tag: React.FC = () => {
+    const {findTag} = useTags();
+    let {id} = useParams<Params>();
+    const tag = findTag(parseInt(id));
     return (
-        <div>hi</div>
+        <div>{tag.name}</div>
     );
 };
 
