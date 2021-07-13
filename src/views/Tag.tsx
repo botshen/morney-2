db21c9ef2f6a6202b3cafb5bc59d802be8909b04
shenxin <363088847@qq.com>
2021-07-13 15:07:29 +0800

    删除 tag

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -5,9 +5,9 @@ import Layout from 'components/Layout';
 import Icon from 'components/Icon';
 import {Button} from 'components/Button';
 import styled from 'styled-components';
-import {Input} from '../components/Input';
-import {Center} from '../components/Center';
-import {Space} from '../components/Space';
+import {Input} from 'components/Input';
+import {Center} from 'components/Center';
+import {Space} from 'components/Space';
 
 type Params = {
     id: string
@@ -26,16 +26,11 @@ const InputWrapper = styled.div`
   margin-top: 8px;
 `;
 const Tag: React.FC = () => {
-    const {findTag, updateTag} = useTags();
+    const {findTag, updateTag, deleteTag} = useTags();
     let {id: idString} = useParams<Params>();
     const tag = findTag(parseInt(idString));
-    return (
-        <Layout>
-            <Topbar>
-                <Icon name="left"/>
-                <span>编辑标签</span>
-                <Icon/>
-            </Topbar>
+    const tagContent = (tag: { id: number; name: string }) => (
+        <div>
             <InputWrapper>
                 <Input label="标签名" type="text" placeholder="标签名"
                        value={tag.name}
@@ -48,8 +43,23 @@ const Tag: React.FC = () => {
                 <Space/>
                 <Space/>
                 <Space/>
-                <Button>删除标签</Button>
+                <Button onClick={() => {
+                    deleteTag(tag.id);
+                }}>删除标签</Button>
             </Center>
+        </div>
+    );
+
+    return (
+        <Layout>
+            <Topbar>
+                <Icon name="left"/>
+                <span>编辑标签</span>
+                <Icon/>
+            </Topbar>
+
+            {tag ? tagContent(tag) : <Center>tag 不存在</Center>}
+
         </Layout>
     );
 };
