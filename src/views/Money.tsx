b0a73dea1cdce8ccc34944aaf2f92c99e74c8fde
shenxin <363088847@qq.com>
2021-07-16 21:22:11 +0800

    展示所有界面

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -11,6 +11,9 @@ const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
 `;
+const CategoryWrapper = styled.div`
+  background: #c4c4c4;
+`;
 
 type Category = '-' | '+'
 
@@ -33,20 +36,17 @@ function Money() {
             setSelected(defaultFormData);
         }
     };
-    useEffect(() => {
-        setTimeout(() => {
-            console.log('时间到');
-            setSelected({...selected, amount: 1000});
-        }, 3000);
-    }, []);
     return (
         <MyLayout>
             <TagsSection value={selected.tagIds}
                          onChange={tagIds => onChange({tagIds})}/>
             <NoteSection value={selected.note}
                          onChange={note => onChange({note})}/>
-            <CategorySection value={selected.category}
-                             onChange={category => onChange({category})}/>
+            <CategoryWrapper>
+                <CategorySection value={selected.category}
+                                 onChange={category => onChange({category})}/>
+            </CategoryWrapper>
+
             <NumberPadSection value={selected.amount}
                               onChange={amount => onChange({amount})}
                               onOk={submit}
